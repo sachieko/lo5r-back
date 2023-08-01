cbe637330a5ba05c1152d214a4d74d0e1226c064
Chelsea Sachieko <arashikabane@gmail.com>
2023-07-31 22:32:09 -0600

    Add choices schema and seeds to database

---
+++ db/schema/3_choices_schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS choices CASCADE;
+
+CREATE TABLE choices (
+  id SERIAL PRIMARY KEY,
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+
+  choice VARCHAR(100) NOT NULL,
+  stat VARCHAR(255) NOT NULL,
+  info TEXT
+);
