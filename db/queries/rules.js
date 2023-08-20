8bab8f73245bae7b4524c4c4e929b493a7950c42
Chelsea Sachieko <arashikabane@gmail.com>
2023-08-19 19:08:02 -0600

    Set up rules schema, route, queries

---
+++ db/queries/rules.js
@@ -0,0 +1,35 @@
+const db = require('../connection');
+const chalk = require('chalk');
+
+const queryRule = async function(id, res) {
+  return db.query(`
+    SELECT rules.title AS title, rules.detail AS detail, rules.imague_url AS image, cards.id AS cardId, 
+    cards.title AS cardTitle, cards.content AS cardContent FROM rules
+    JOIN cards ON rules.id = rule_id
+    WHERE rule_id = $1
+    ORDER BY cardsId;`, [id], (err, result) => {
+      if (err) {
+        console.error(chalk.red('Error executing query:'), err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      // console.log(chalk.green('Success: Route Rules with param! Results:', result.rows.length));
+      return res.json(result.rows);
+    });
+};
+
+const queryRules = async function(res) {
+  return db.query(`
+  SELECT id, title, detail, image_url FROM rules`, 
+  (err, result) => {
+    if (err) {
+      console.error(chalk.red('Error executing query:'), err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    // console.log(chalk.green('Success: Route Questions without Params!'));
+    return res.json(result.rows);
+  });
+};
+
+module.exports = { queryRule, queryRules };
---
+++ src/routes/rules.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { queryRule, queryRules } = require('../../db/queries/rules');
+
+// GET /rules/id
+router.get('/:id', async(req, res) => {
+  const id = req.params.id;
+  queryRule(id, res);
+});
+
+// GET /rules
+router.get('/', async(req, res) => {
+  queryRules(res);
+});
+
+module.exports = router;
