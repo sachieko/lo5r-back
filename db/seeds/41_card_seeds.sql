2e194ebeb1a095617217b6d7393bd3b2992076eb
Chelsea Sachieko <arashikabane@gmail.com>
2023-08-30 01:02:02 -0600

    Separate rule and lore cards into their own seed files for organization

---
+++ db/seeds/41_card_seeds.sql
@@ -0,0 +1,41 @@
+-- For cards that relate to rules only in these seeds
+INSERT INTO cards (rule_id, title, content)
+VALUES
+-- Rule 1: Rolling
+(1, 'Resolving Explosive Successes', 'For any explosive successes that were kept, roll another die of the same type. Then you can choose whether to keep that additional rolled die after, and if it is also an explosive success you can roll once more'),
+(1, 'Resolving Strife', 'For each strife symbol on kept die, record an additional strife on your character sheet.'),
+(1, 'Resolving Opportunities', 'Players will often have techniques as well as default options to spend opportunity on a check. This often prompts players and the Storyteller to create more narrative effects in a scene or add additional details for the players. Some techniques use it to alter the effects of the technique as well.'),
+(1, 'Resolving Successes', 'If the total number of successes meets the TN (Target number) of the check, the character succeeds. Successes that exceed the TN are bonus successes and may increase the degree of success.'),
+-- Rule 2: Conflict
+(2, 'Step One:', 'This page is a work in progress! Sorry!'),
+-- Rule 3: Skill Approach Quick Reference Cards
+(3, 'Artisan Approaches', 
+'Earth: Restore (a work, an item).
+Water: Adapt (a work, an item).
+Fire: Invent (a work, an item).
+Air: Refine (a work, an item).
+Void: Attune (to a work, an item).'),
+(3, 'Social Approaches', 
+'Earth: Reason (with people using logic).
+Water: Charm (people to be favorable towards you).
+Fire: Incite (people to act on emotions).
+Air: Trick (people into believing something).
+Void: Enlighten (people to understand something fundamental)'),
+(3,'Scholar Approaches', 
+'Earth: Recall (information).
+Water: Survey (surroundings for information).
+Fire: Theorize (about the information).
+Air: Analyze (details of information).
+Void: Sense (with a hunch).'),
+(3, 'Martial Approaches', 
+'Earth: Withstand (a force).
+Water: Shift (a force against itself).
+Fire: Overwhelm (a force).
+Air: Feint (to lure an opposing force).
+Void: Sacrifice (give to achieve a greater end).'),
+(3, 'Trade Approaches', 
+'Earth: Produce (resources you need through work).
+Water: Exchange (labor or resources for others).
+Fire: Innovate (a way to get resources).
+Air: Con (to get resources for nothing).
+Void: Subsist (to get resources from the environment without disrupting it).');
---
+++ db/seeds/42_card_seeds.sql
@@ -0,0 +1,11 @@
+-- For cards that relate to lore only in these seeds
+INSERT INTO cards (lore_id, title, content)
+VALUES
+-- 1 Lore: Bushido
+(1, 'Compassion (仁, Jin)', 'Through intense training and hard work the true warrior becomes quick and strong. They are not as most people. They develop a power that must be used for good. They have compassion. They help their fellow men at every opportunity. If an opportunity does not arise, they go out of their way to find one.'),
+(1, ' Courage (勇, Yu)', 'Hiding like a turtle in a shell is not living at all. A true warrior must have heroic courage. It is absolutely risky. It is living life completely, fully and wonderfully. Heroic courage is not blind. It is intelligent and strong.'),
+(1, ' Respect or Courtesy (礼, Rei)', 'True warriors have no reason to be cruel. They do not need to prove their strength. Warriors are not only respected for their strength in battle, but also by their dealings with others. The true strength of a warrior becomes apparent during difficult times.'),
+(1, ' Duty and Loyalty (忠義, Chūgi)', 'Warriors are responsible for everything that they have done and everything that they have said and all of the consequences that follow. They are immensely loyal to all of those in their care. To everyone that they are responsible for, they remain fiercely true.'),
+(1, ' Honor  (名誉, Meiyo)', 'Warriors have only one judge of honor and character, and this is themselves. Decisions they make and how these decisions are carried out are a reflection of who they truly are. You cannot hide from yourself.'),
+(1, ' Righteousness (義, Gi)', 'Be acutely honest throughout your dealings with all people. Believe in justice, not from other people, but from yourself. To the true warrior, all points of view are deeply considered regarding honesty, justice and integrity. Warriors make a full commitment to their decisions.'),
+(1, ' Sincerity (誠, Makoto)', 'When warriors say that they will perform an action, it is as good as done. Nothing will stop them from completing what they say they will do. They do not have to ''give their word''. They do not have to ''promise''. Speaking and doing are the same action.');
