466e7709dfed15caf3d1caf8c1ee7357fb9abd86
Chelsea Sachieko <arashikabane@gmail.com>
2023-08-30 01:22:25 -0600

    Add currency lore

--- db/seeds/42_card_seeds.sql
+++ db/seeds/42_card_seeds.sql
@@ -8,4 +8,10 @@ VALUES
 (1, ' Duty and Loyalty (忠義, Chūgi)', 'Warriors are responsible for everything that they have done and everything that they have said and all of the consequences that follow. They are immensely loyal to all of those in their care. To everyone that they are responsible for, they remain fiercely true.'),
 (1, ' Honor  (名誉, Meiyo)', 'Warriors have only one judge of honor and character, and this is themselves. Decisions they make and how these decisions are carried out are a reflection of who they truly are. You cannot hide from yourself.'),
 (1, ' Righteousness (義, Gi)', 'Be acutely honest throughout your dealings with all people. Believe in justice, not from other people, but from yourself. To the true warrior, all points of view are deeply considered regarding honesty, justice and integrity. Warriors make a full commitment to their decisions.'),
-(1, ' Sincerity (誠, Makoto)', 'When warriors say that they will perform an action, it is as good as done. Nothing will stop them from completing what they say they will do. They do not have to ''give their word''. They do not have to ''promise''. Speaking and doing are the same action.');
+(1, ' Sincerity (誠, Makoto)', 'When warriors say that they will perform an action, it is as good as done. Nothing will stop them from completing what they say they will do. They do not have to ''give their word''. They do not have to ''promise''. Speaking and doing are the same action.'),
+-- 2 Lore: Koku
+(2, 'Exchange Rates', 'Generally: 
+1 koku = 5 bu
+1 koku = 50 zeni
+1 bu = 10 zeni
+This of course can fluctuate based on which clan minted it and where you are.');
