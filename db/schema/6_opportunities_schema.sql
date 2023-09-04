e179b146670dfa29fdf06925c02750df2d101c2d
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-03 19:27:19 -0600

    Fix typo in schema

--- db/schema/6_opportunities_schema.sql
+++ db/schema/6_opportunities_schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS opportunities CASCADE;
 CREATE TABLE opportunities (
   id SERIAL PRIMARY KEY,
   technique_id INTEGER NULL,
-  FOREIGN KEY (technique_id) REFERENCES technique(id) ON DELETE CASCADE,
+  FOREIGN KEY (technique_id) REFERENCES techniques(id) ON DELETE CASCADE,
 
   ring VARCHAR(30) NOT NULL,
   category VARCHAR(30) NOT NULL,
