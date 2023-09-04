128f13cad9c60d3a36c1f35d5c65dedd93c6f336
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-04 00:59:27 -0600

    DRY code formatting for rules

--- db/queries/rules.js
+++ db/queries/rules.js
@@ -1,30 +1,29 @@
 const db = require('../connection');
 const chalk = require('chalk');
 
-const queryRule = async function(id, res) {
-  try {
-    const result = await db.query(`
-      SELECT rules.title AS title, rules.detail AS detail, rules.image_url AS image, cards.id AS id, 
-      cards.title AS header, cards.content AS content FROM rules
-      JOIN cards ON rules.id = rule_id
-      WHERE rule_id = $1
-      ORDER BY cards.id;`, [id]);
-    // console.log(chalk.green('Success: Route Rules with param! Results:', result.rows.length));
-    return res.json(result.rows);
-  } catch (error) {
-    console.error(chalk.red('Error executing query:'), error);
-    res.status(500).send('Internal Server Error');
-  }
-};
+/*
 
-const queryRules = async function(res) {
-  try {
-    const result = [];
+  Rule data format:
+  { 
+    id: number,
+    title: string,
+    category: string,
+    detail: string,
+    image: url string,
+    cards: [ 
+      {
+        id: number,
+        header: string,
+        content: string,
+      }, ...
+    ]
+  }
+
+*/
+
+const formatRuleResult = function(ruleResult) {
+  const result = [];
     let currentRule = null;
-    const ruleResult = await db.query(`
-      SELECT rules.* , cards.id AS card_id, cards.title AS header, cards.content AS content FROM rules
-      JOIN cards ON rules.id = rule_id
-      ORDER BY rules.id, cards.id;`);
     for (const row of ruleResult.rows) {
       // If there isn't a current rule or the id has changed, make a new currentRow and add to the result array
       if (currentRule === null || currentRule.id !== row.id) {
@@ -48,7 +47,31 @@ const queryRules = async function(res) {
         });
       }
     }
-    return res.json(result);
+    return result;
+}
+
+const queryRule = async function(id, res) {
+  try {
+    const ruleResult = await db.query(`
+      SELECT rules.title AS title, rules.detail AS detail, rules.image_url AS image, cards.id AS id, 
+      cards.title AS header, cards.content AS content FROM rules
+      JOIN cards ON rules.id = rule_id
+      WHERE rule_id = $1
+      ORDER BY cards.id;`, [id]);
+    return res.json(formatRuleResult(ruleResult));
+  } catch (error) {
+    console.error(chalk.red('Error executing query:'), error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+const queryRules = async function(res) {
+  try {
+    const ruleResult = await db.query(`
+      SELECT rules.* , cards.id AS card_id, cards.title AS header, cards.content AS content FROM rules
+      JOIN cards ON rules.id = rule_id
+      ORDER BY rules.id, cards.id;`);
+    return res.json(formatRuleResult(ruleResult));
   } catch (error) {
     console.error(chalk.red('Error executing query:'), error);
     res.status(500).send('Internal Server Error');
