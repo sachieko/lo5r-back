586ab45ed194d2550bc7437273a084ff8075628f
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-04 03:07:08 -0600

    Create opportunity queries

--- db/queries/opportunities.js
+++ db/queries/opportunities.js
@@ -4,12 +4,12 @@ const queryOpportunity = async function(id, res) {
   try {
     const oppResult = await db.query(`
       SELECT opportunities.*, techniques.name, techniques.prerequisite, techniques.rank,
-      techniques.category AS type, techniques.description, techniques.activation, techniques.effect AS tech_effect
+      techniques.category AS type, techniques.description, techniques.activation, techniques.effect AS technique_effect
        FROM opportunities
       JOIN techniques ON technique_id = techniques.id
       WHERE id = $1
       LIMIT 1;`, [id]);
-    // There should only be 1 result at this point.
+    // There should only be 1 result at most.
     const result = [...oppResult.rows[0]];
     return res.json(result);
   } catch (error) {
@@ -21,7 +21,7 @@ const queryOpportunities = async function(res) {
   try {
     const oppResult = await db.query(`
       SELECT opportunities.*, techniques.name, techniques.prerequisite, techniques.rank,
-      techniques.category AS type, techniques.description, techniques.activation, techniques.effect AS tech_effect
+      techniques.type, techniques.description, techniques.activation, techniques.effect AS technique_effect
        FROM opportunities
       JOIN techniques ON technique_id = techniques.id
       ORDER BY opportunities.id, opportunities.category, opportunities.ring;`, [id]);
