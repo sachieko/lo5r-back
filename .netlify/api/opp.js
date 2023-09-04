78f60b7bbb2b3ff8a0d7f474cb7ca5db682c8f3c
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-04 03:47:57 -0600

    Add opportunities netlify function

---
+++ .netlify/api/opp.js
@@ -0,0 +1,61 @@
+const db = require('../../db/connection');
+
+const HOSTURL = process.env.HOSTURL;
+const headers = {
+  'Access-Control-Allow-Origin': HOSTURL, 
+  'Access-Control-Allow-Headers': 'Content-Type'
+};
+
+exports.handler = async (event, context) => {
+  const id = event.path.split('/').pop();
+  const path = event.path;
+
+  if (path === '/.netlify/functions/opp') {
+    try {
+      const oppResult = await db.query(`
+      SELECT opportunities.*, techniques.name, techniques.prerequisite, techniques.rank,
+      techniques.type, techniques.description, techniques.activation, techniques.effect AS technique_effect
+      FROM opportunities
+      LEFT JOIN techniques ON technique_id = techniques.id
+      ORDER BY opportunities.id, opportunities.category, opportunities.ring;`);
+      const result = oppResult.rows;
+
+      return {
+        headers,
+        statusCode: 200,
+        body: JSON.stringify(result)
+      };
+    } catch (error) {
+      return {
+        headers,
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' })
+      };
+    }
+  }
+
+  if (path.startsWith('/.netlify/functions/opp/')) {
+    try {
+      const oppResult = await db.query(`
+      SELECT opportunities.*, techniques.name, techniques.prerequisite, techniques.rank,
+      techniques.type, techniques.description, techniques.activation, techniques.effect AS technique_effect
+      FROM opportunities
+      LEFT JOIN techniques ON technique_id = techniques.id
+      WHERE opportunities.id = $1
+      LIMIT 1;`, [id]);
+      const result = oppResult.rows;
+      
+      return {
+        headers,
+        statusCode: 200,
+        body: JSON.stringify(result)
+      };
+    } catch (error) {
+      return {
+        headers,
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' })
+      };
+    }
+  }
+};
