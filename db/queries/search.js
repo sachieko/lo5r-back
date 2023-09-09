779b062a76b8d33834030c0a31b56933061cc277
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-08 22:15:26 -0600

    Fix techniques in search query, format with links

--- db/queries/search.js
+++ db/queries/search.js
@@ -3,20 +3,20 @@ const db = require('../connection');
 // @Params: String is the search string to look for matches in lore, rules, techniques, etc.
 const querySearch = async function(string, res) {
   const query = `
-    SELECT id, title, detail FROM rules 
+    SELECT '/rules/' || id AS link, title, detail FROM rules 
     WHERE title ILIKE $1 || '%'
     UNION 
-    SELECT id, title, detail FROM lore
+    SELECT '/lore/' || id AS link, title, detail FROM lore
     WHERE title ILIKE $1 || '%'
     UNION
-    SELECT id, name AS title, category AS detail FROM techniques
+    SELECT '/techniques/' || id AS link, name AS title, type AS detail FROM techniques
     WHERE name ILIKE $1 || '%';
     `;
   try {
     const result = await db.query(query, [string]);
     return res.json(result.rows)
   } catch (error) {
-    res.status(500).send(`Internal Server Error!`);
+    res.status(500).send(error);
   }
 };
 
