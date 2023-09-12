fd6fe4d848ea4fcac2f6ed21fc375d72c139ee7d
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-12 00:25:49 -0600

    Change parameter to array, really embarrassing..

--- .netlify/api/search.js
+++ .netlify/api/search.js
@@ -9,29 +9,20 @@ const headers = {
 
 exports.handler = async (event, context) => {
   const searchString = event.queryStringParameters.q;
-
+    const query = `
+    SELECT '/rules/' || id AS link, title, detail FROM rules 
+    WHERE title '%' || ILIKE $1 || '%'
+    UNION 
+    SELECT '/lore/' || id AS link, title, detail FROM lore
+    WHERE title '%' || ILIKE $1 || '%'
+    UNION
+    SELECT '/techniques/' || id AS link, name AS title, type AS detail FROM techniques
+    WHERE name '%' || ILIKE $1 || '%';
+    `;
   try {
-      const result = await db.query(`
-      SELECT '/rules/' || id AS link, title, detail FROM rules 
-      WHERE title '%' || ILIKE $1 || '%'
-      UNION 
-      SELECT '/lore/' || id AS link, title, detail FROM lore
-      WHERE title '%' || ILIKE $1 || '%'
-      UNION
-      SELECT '/techniques/' || id AS link, name AS title, type AS detail FROM techniques
-      WHERE name '%' || ILIKE $1 || '%';
-      `, [searchString]);
-      
-      return {
-        headers,
-        statusCode: 200,
-        body: JSON.stringify(result.rows)
-      };
-    } catch (error) {
-      return {
-        headers,
-        statusCode: 500,
-        body: JSON.stringify({ error: 'Internal Server Error' })
-      };
+    const result = await db.query(query, [searchString]);
+    return res.json(result.rows)
+  } catch (error) {
+    res.status(500).send(error);
   }
 };
