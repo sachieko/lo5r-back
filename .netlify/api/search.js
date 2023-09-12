7882567f07722d2aa9020602d613d1d552fe20f1
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-12 00:33:21 -0600

    Fix error in search query

--- .netlify/api/search.js
+++ .netlify/api/search.js
@@ -9,15 +9,15 @@ const headers = {
 
 exports.handler = async (event, context) => {
   const searchString = event.queryStringParameters.q;
-    const query = `
+  const query = `
     SELECT '/rules/' || id AS link, title, detail FROM rules 
-    WHERE title '%' || ILIKE $1 || '%'
+    WHERE title ILIKE '%' || $1 || '%'
     UNION 
     SELECT '/lore/' || id AS link, title, detail FROM lore
-    WHERE title '%' || ILIKE $1 || '%'
+    WHERE title ILIKE '%' || $1 || '%'
     UNION
     SELECT '/techniques/' || id AS link, name AS title, type AS detail FROM techniques
-    WHERE name '%' || ILIKE $1 || '%';
+    WHERE name ILIKE '%' || $1 || '%';
     `;
   try {
     const result = await db.query(query, [searchString]);
