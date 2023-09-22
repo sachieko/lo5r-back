c40a31f0d45ac89c884021029f2e0aeadfd456e3
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-21 21:08:08 -0600

    Add missing LEFT to LEFT JOIN in query"

--- db/queries/questions.js
+++ db/queries/questions.js
@@ -5,7 +5,7 @@ const queryQuestion = async function(id, res) {
   try {
     const questionResult = await db.query(`
       SELECT questions.*, choices.id AS choice_id, choices.stat, choices.info AS choiceinfo, choices.choice FROM questions
-      JOIN choices ON questions.id = question_id
+      LEFT JOIN choices ON questions.id = question_id
       WHERE question_id = $1
       ORDER BY choices.id;`, [id]);
 
@@ -22,7 +22,7 @@ const queryQuestions = async function(res) {
     const questionResult = await db.query(`
       SELECT questions.*, choices.id AS choice_id, choices.choice,
       choices.stat, choices.info AS choiceinfo FROM questions
-      JOIN choices ON questions.id = question_id
+      LEFT JOIN choices ON questions.id = question_id
       ORDER BY questions.id, choices.id;`);
     
     return res.json(formatQuestionResult(questionResult));
