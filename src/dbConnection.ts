ad50e611e2d13c6d5ec3cb31de25b0b98fc3eb1a
Chelsea Sachieko <arashikabane@gmail.com>
2025-02-11 08:11:16 -0700

    migrate db connection to typescript

---
+++ src/dbConnection.ts
@@ -0,0 +1,26 @@
+import { apiParamtypes } from "./types/dataTypes";
+
+// PG database client/connection setup
+import { Pool } from "pg";
+import fs from "fs";
+
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync("./ssl/root.crt").toString(),
+    key: fs.readFileSync("./ssl/server.key").toString(),
+    cert: fs.readFileSync("./ssl/server.crt").toString(),
+  },
+});
+
+export const db = {
+  query: async (text: string, params?: apiParamtypes) => {
+    const client = await pool.connect();
+    try {
+      return await client.query(text, params);
+    } finally {
+      client.release();
+    }
+  },
+};
