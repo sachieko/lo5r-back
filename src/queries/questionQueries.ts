b95caf44589cb3c1de677472a4b1abf9f1eaa863
Chelsea Sachieko <arashikabane@gmail.com>
2025-02-11 22:14:49 -0700

    Add missing comma to SQL query

--- src/queries/questionQueries.ts
+++ src/queries/questionQueries.ts
@@ -1,12 +1,11 @@
 export const questionsQuery = `
   SELECT questions.*, JSON_AGG(JSON_BUILD_OBJECT(
-    'choice_id', choices.id,
-    'choice', choices.choice
-    'stat', choices.stat,
-    'info', choices.info,
-  )) AS choices
-  FROM questions
-  LEFT JOIN choices ON question_id = questions.id
+  'choice_id', choices.id,
+  'choice', choices.choice,
+  'stat', choices.stat,
+  'info', choices.info
+  )) AS choices FROM questions
+  LEFT JOIN choices ON questions.id = question_id
   GROUP BY questions.id
   ORDER BY questions.id;`;
 
@@ -20,5 +19,6 @@ export const questionsIdQuery = `
   LEFT JOIN choices ON questions.id = question_id
   WHERE questions.id = $1
   GROUP BY questions.id
-  ORDER BY questions.id, choices.id
+  ORDER BY questions.id
   LIMIT 1;`; 
+  
