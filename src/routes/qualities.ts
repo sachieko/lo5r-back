b34e2cb8ac1848488d49a9bc24c429ee48b2261b
Chelsea Sachieko <arashikabane@gmail.com>
2025-02-11 21:45:35 -0700

    Lint

--- src/routes/qualities.ts
+++ src/routes/qualities.ts
@@ -1,16 +1,16 @@
-import { Request, Response, Router } from "express"
+import { Request, Response, Router } from "express";
 import wolfQueryHandler from "../queries/wolfQueryHandler";
 const router = Router();
 import { qualitiesIdQuery, qualitiesQuery } from "../queries/qualityQueries";
 // GET /conditions/id
 router.get("/:id", async (req: Request, res: Response) => {
   const id = req.params.id;
-  wolfQueryHandler(res, qualitiesIdQuery, [id])
+  wolfQueryHandler(res, qualitiesIdQuery, [id]);
 });
 
 // GET /conditions
 router.get("/", async (req: Request, res: Response) => {
-  wolfQueryHandler(res, qualitiesQuery)
+  wolfQueryHandler(res, qualitiesQuery);
 });
 
 export default router;
