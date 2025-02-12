8061829713b24bc77922eee0ddef1d88725f1af1
Chelsea Sachieko <arashikabane@gmail.com>
2025-02-11 22:06:09 -0700

    Update package version, rewrite question query

--- src/queries/questionQueries.ts
+++ src/queries/questionQueries.ts
@@ -1,13 +1,14 @@
 export const questionsQuery = `
   SELECT questions.*, JSON_AGG(JSON_BUILD_OBJECT(
-  'choice_id', choices.id,
-  'choice', choices.choice,
-  'stat', choices.stat,
-  'info', choices.info
-  )) AS choices FROM questions
-  LEFT JOIN choices ON questions.id = question_id
+    'choice_id', choices.id,
+    'choice', choices.choice
+    'stat', choices.stat,
+    'info', choices.info,
+  )) AS choices
+  FROM questions
+  LEFT JOIN choices ON question_id = questions.id
   GROUP BY questions.id
-  ORDER BY questions.id, choices.id;`;
+  ORDER BY questions.id;`;
 
 export const questionsIdQuery = `
   SELECT questions.*, JSON_AGG(JSON_BUILD_OBJECT(
