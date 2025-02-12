3526a55face8de5a4bc3aee788ba4f45f320624d
Chelsea Sachieko <arashikabane@gmail.com>
2025-02-11 20:08:45 -0700

    Remove ssl until ca signing complete and figured out ecksdee

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -2,16 +2,18 @@ import { apiParamtypes } from "./types/dataTypes";
 
 // PG database client/connection setup
 import { Pool } from "pg";
-import fs from "fs";
+// import fs from "fs";
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
+  /* 
   ssl: {
     rejectUnauthorized: false,
     ca: fs.readFileSync("./ssl/root.crt").toString(),
     key: fs.readFileSync("./ssl/server.key").toString(),
     cert: fs.readFileSync("./ssl/server.crt").toString(),
   },
+  */
 });
 
 export const db = {
