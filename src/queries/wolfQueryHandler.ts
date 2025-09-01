9b2649ea9bae834f6a0266c0000edb2db81b8e80
Chelsea Sachieko <dev@yuseiko.org>
2025-09-01 02:34:09 -0600

    Improve error message on queryHandler

--- src/queries/wolfQueryHandler.ts
+++ src/queries/wolfQueryHandler.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import { apiParamtypes } from "../types/dataTypes";
 import { db } from "../dbConnection";
+import chalk from "chalk";
 
 // generic query handler that will contact the database, then return the result for a route
 const wolfQueryHandler = async (
@@ -13,6 +14,7 @@ const wolfQueryHandler = async (
     return res.json(result.rows);
   } catch (error) {
     res.status(500).send("Internal Server Error - @yuseiko.bsky.social");
+    console.error(chalk.red('Database Error: SQL Query: ' + queryString + '\n'), error);
   }
 };
 
