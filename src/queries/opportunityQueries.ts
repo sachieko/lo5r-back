a96f340a455330e9f7d5fcc509dc03d98c07bb5a
Chelsea Sachieko <dev@yuseiko.org>
2025-09-01 08:59:17 -0600

    Fix opportunity query bug

--- src/queries/opportunityQueries.ts
+++ src/queries/opportunityQueries.ts
@@ -1,12 +1,12 @@
 export const opportunitiesQuery = `
-  SELECT opportunities.*, '[' || techniques.name || ']', techniques.prerequisite, techniques.rank,
+  SELECT opportunities.*, '[' || techniques.name || ']' AS name, techniques.prerequisite, techniques.rank,
   techniques.type, techniques.description, techniques.activation, techniques.effect AS technique_effect
   FROM opportunities
   LEFT JOIN techniques ON technique_id = techniques.id
   ORDER BY opportunities.ring, opportunities.category, opportunities.id;`;
 
 export const opportunityQuery = `
-  SELECT opportunities.*, '[' || techniques.name || ']', techniques.prerequisite, techniques.rank,
+  SELECT opportunities.*, '[' || techniques.name || ']' AS name, techniques.prerequisite, techniques.rank,
   techniques.type, techniques.description, techniques.activation, techniques.effect AS technique_effect
   FROM opportunities
   LEFT JOIN techniques ON technique_id = techniques.id
