3cc68833bf92aedd7e3ce4e04c9f3db804c68636
Chelsea Sachieko <dev@yuseiko.org>
2026-02-24 04:47:39 -0700

    update query handler type for lore.ts

--- src/routes/lore.ts
+++ src/routes/lore.ts
@@ -6,7 +6,7 @@ import { loreQuery, loreIdQuery } from "../queries/loreQueries";
 // GET /lore/id
 router.get("/:id", async (req: Request, res: Response) => {
   const id = req.params.id;
-  wolfQueryHandler(res, loreIdQuery, [id.toString()]);
+  wolfQueryHandler(res, loreIdQuery, [Number(id)]);
 });
 
 // GET /lore
